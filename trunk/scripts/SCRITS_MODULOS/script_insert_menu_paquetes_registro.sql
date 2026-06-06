915596730ddc8f886a5ee13b82cb6b6fcb929cc6
jesus velazquez <jvl.24.12.88@gmail.com>
2026-06-06 01:36:26 -0600

    ajuste

--- trunk/scripts/SCRITS_MODULOS/script_insert_menu_paquetes_registro.sql
+++ trunk/scripts/SCRITS_MODULOS/script_insert_menu_paquetes_registro.sql
@@ -7,12 +7,12 @@ VALUES
 /*******************PRIVILEGIOS*************************************************/
 INSERT INTO menus(id_menu,id_menu_padre,codigo_menu,nombre_menu,descripcion_menu,tipo_menu,url_menu,menu_icono,menu_activo,num_orden,id_grupo_menu)
 VALUES
-(NEXTVAL('seq_menus'),(SELECT id_menu FROM menus WHERE codigo_menu = 'PRIV_REG_PAQUETES'),'PRIV_BTN_NVO_NOTIFICACION','Botón para registrar nueva notificación',
-'Botón para registrar nueva notificación','PRIVILEGIO','#','',1,10,'PRIV_ESPECIFICOS'),
-(NEXTVAL('seq_menus'),(SELECT id_menu FROM menus WHERE codigo_menu = 'PRIV_REG_PAQUETES'),'PRIV_BTN_EDI_NOTIFICACION','Botón para editar registro de notificación',
-'Botón para editar registro de notificación','PRIVILEGIO','#','',1,11,'PRIV_ESPECIFICOS'),
-(NEXTVAL('seq_menus'),(SELECT id_menu FROM menus WHERE codigo_menu = 'PRIV_REG_PAQUETES'),'PRIV_BTN_CANL_NOTIFICACION','Botón para cancelar registro de notificación',
-'Botón para cancelar registro de notificación','PRIVILEGIO','#','',1,12,'PRIV_ESPECIFICOS');
+(NEXTVAL('seq_menus'),(SELECT id_menu FROM menus WHERE codigo_menu = 'PRIV_REG_PAQUETES'),'PRIV_BTN_NVO_PAQUETE','Botón para registrar nuevo paquete',
+'Botón para registrar nuevo paquete','PRIVILEGIO','#','',1,10,'PRIV_ESPECIFICOS'),
+(NEXTVAL('seq_menus'),(SELECT id_menu FROM menus WHERE codigo_menu = 'PRIV_REG_PAQUETES'),'PRIV_BTN_EDI_PAQUETE','Botón para editar registro de paquete',
+'Botón para editar registro de paquete','PRIVILEGIO','#','',1,11,'PRIV_ESPECIFICOS'),
+(NEXTVAL('seq_menus'),(SELECT id_menu FROM menus WHERE codigo_menu = 'PRIV_REG_PAQUETES'),'PRIV_BTN_ELIM_PAQUETE','Botón para eliminar registro de paquete',
+'Botón para eliminar registro de paquete','PRIVILEGIO','#','',1,12,'PRIV_ESPECIFICOS');
 
 /*---Rol Privilegio del Modulo--*/
 INSERT INTO roles_privilegios (id_rol_privilegio,id_rol,id_menu,solo_lectura,lectura_escritura)
@@ -21,6 +21,6 @@ VALUES
 
 INSERT INTO roles_privilegios (id_rol_privilegio,id_rol,id_menu,solo_lectura,lectura_escritura)
 VALUES
-(NEXTVAL('seq_roles_privilegios'), 1,(SELECT id_menu FROM menus WHERE codigo_menu = 'PRIV_BTN_NVO_NOTIFICACION'),0,1),
-(NEXTVAL('seq_roles_privilegios'), 1,(SELECT id_menu FROM menus WHERE codigo_menu = 'PRIV_BTN_EDI_NOTIFICACION'),0,1),
-(NEXTVAL('seq_roles_privilegios'), 1,(SELECT id_menu FROM menus WHERE codigo_menu = 'PRIV_BTN_CANL_NOTIFICACION'),0,1);
+(NEXTVAL('seq_roles_privilegios'), 1,(SELECT id_menu FROM menus WHERE codigo_menu = 'PRIV_BTN_NVO_PAQUETE'),0,1),
+(NEXTVAL('seq_roles_privilegios'), 1,(SELECT id_menu FROM menus WHERE codigo_menu = 'PRIV_BTN_EDI_PAQUETE'),0,1),
+(NEXTVAL('seq_roles_privilegios'), 1,(SELECT id_menu FROM menus WHERE codigo_menu = 'PRIV_BTN_ELIM_PAQUETE'),0,1);
