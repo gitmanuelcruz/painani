1c08e0326fc8b00fc794734ac7155ff5afdf6a51
jesus velazquez <jvl.24.12.88@gmail.com>
2026-06-24 23:15:51 -0600

    ajuste

--- trunk/scripts/PROCEDURE/FN_VALID_OFICIOS.sql
+++ trunk/scripts/PROCEDURE/FN_VALID_OFICIOS.sql
@@ -7,14 +7,44 @@ DECLARE
 	v_retorno INTEGER := 0;
 			
 BEGIN
-	/*-------------VALIDACION ----------------*/
+	/*-------------VALIDACION NUM ORDEN ----------------*/
 	UPDATE notificaciones_tmp a SET
 		observaciones = COALESCE(a.observaciones,'')||'El número de orden es requerido, '
+	WHERE LENGTH(a.num_orden) = 0
+	AND a.usuario = p_usuario;
+
+	UPDATE notificaciones_tmp a SET
+		observaciones = COALESCE(a.observaciones,'')||'El número de orden ('||a.num_orden||') esta duplicado en el archivo, '
+	WHERE LENGTH(a.num_orden) > 0
+	AND EXISTS (
+		SELECT NULL
+		FROM notificaciones_tmp x
+		WHERE UPPER(TRIM(x.num_orden)) = UPPER(TRIM(a.num_orden))
+		AND x.usuario = p_usuario
+		GROUP BY x.num_orden
+		HAVING COUNT(*) > 1
+	)
+	AND a.usuario = p_usuario;
+
+	UPDATE notificaciones_tmp a SET
+		observaciones = COALESCE(a.observaciones,'')||'El número de orden ('||a.num_orden||') ya se encuentra registrado, '
+	WHERE LENGTH(a.num_orden) > 0
+	AND EXISTS (
+		SELECT NULL
+		FROM notificaciones x
+		WHERE x.id_estatus_notificacion <> 'CANCELADO'
+		AND UPPER(TRIM(x.num_orden)) = UPPER(TRIM(a.num_orden))
+	)
+	AND a.usuario = p_usuario;
+
+	/*-------------VALIDACION NUM OFICIO----------------*/
+	UPDATE notificaciones_tmp a SET
+		observaciones = COALESCE(a.observaciones,'')||'El número de oficio es requerido, '
 	WHERE LENGTH(a.num_oficio) = 0
 	AND a.usuario = p_usuario;
 
 	UPDATE notificaciones_tmp a SET
-		observaciones = COALESCE(a.observaciones,'')||'El número de orden ('||a.num_oficio||') esta duplicado en el archivo, '
+		observaciones = COALESCE(a.observaciones,'')||'El número de oficio ('||a.num_oficio||') esta duplicado en el archivo, '
 	WHERE LENGTH(a.num_oficio) > 0
 	AND EXISTS (
 		SELECT NULL
@@ -27,7 +57,7 @@ BEGIN
 	AND a.usuario = p_usuario;
 
 	UPDATE notificaciones_tmp a SET
-		observaciones = COALESCE(a.observaciones,'')||'El número de orden ('||a.num_oficio||') ya se encuentra registrado, '
+		observaciones = COALESCE(a.observaciones,'')||'El número de oficio ('||a.num_oficio||') ya se encuentra registrado, '
 	WHERE LENGTH(a.num_oficio) > 0
 	AND EXISTS (
 		SELECT NULL
@@ -49,20 +79,40 @@ BEGIN
 	AND (isvaliddate2(a.fecha_oficio) = 0 OR isNumerico(a.fecha_oficio) = TRUE)
 	AND a.usuario = p_usuario;
 
-	/*-------------VALIDACION DE PRIORIDAD----------------*/
+	/*-------------VALIDACION DE ID INSUMO----------------*/
 	UPDATE notificaciones_tmp a SET
-		observaciones = COALESCE(a.observaciones,'')||'La prioridad del número de orden es requerido, '
-	WHERE LENGTH(a.prioridad) = 0
+		observaciones = COALESCE(a.observaciones,'')||'El ID insumo es requerido, '
+	WHERE LENGTH(a.id_insumo) = 0
 	AND a.usuario = p_usuario;
 
 	UPDATE notificaciones_tmp a SET
-		observaciones = COALESCE(a.observaciones,'')||'La prioridad no existe, '
-	WHERE LENGTH(a.prioridad) > 0
-	AND NOT EXISTS (
-		SELECT NULL
-		FROM prioridades x
-		WHERE UPPER(TRIM(x.id_prioridad)) = UPPER(TRIM(a.prioridad))
-	)
+		observaciones = COALESCE(a.observaciones,'')||'El ID insumo es invalido debe ser numérico,'
+	WHERE LENGTH(a.id_insumo) > 0
+	AND isNumerico(a.id_insumo) = FALSE
+	AND a.usuario = p_usuario;
+
+	/*-------------VALIDACION DE ID BLOQUE----------------*/
+	UPDATE notificaciones_tmp a SET
+		observaciones = COALESCE(a.observaciones,'')||'El ID bloque es requerido, '
+	WHERE LENGTH(a.id_bloque) = 0
+	AND a.usuario = p_usuario;
+
+	UPDATE notificaciones_tmp a SET
+		observaciones = COALESCE(a.observaciones,'')||'El ID bloque es invalido debe ser numérico,'
+	WHERE LENGTH(a.id_bloque) > 0
+	AND isNumerico(a.id_bloque) = FALSE
+	AND a.usuario = p_usuario;
+
+	/*-------------VALIDACION DE MONTO PRESUNTIVA----------------*/
+	UPDATE notificaciones_tmp a SET
+		observaciones = COALESCE(a.observaciones,'')||'El monto presuntiva es requerido,'
+	WHERE LENGTH(a.monto_presuntiva) = 0
+	AND a.usuario = p_usuario;
+	
+	UPDATE notificaciones_tmp a SET
+		observaciones = COALESCE(a.observaciones,'')||'El monto presuntiva es invalido,'
+	WHERE LENGTH(a.monto_presuntiva) > 0
+	AND isNumerico(REPLACE(a.monto_presuntiva,'.','')) = FALSE
 	AND a.usuario = p_usuario;
 
 	/*-------------VALIDACION DE DOMICILIO----------------*/
@@ -79,6 +129,19 @@ BEGIN
 
 	/**************************************************************************************************************/
 	SELECT COUNT(*) INTO v_validador FROM notificaciones_tmp WHERE LENGTH(observaciones) > 0 AND usuario = p_usuario;
+
+	IF COALESCE(v_validador,0) = 0 
+		THEN
+			UPDATE notificaciones_tmp x SET
+				prioridad = y.id_prioridad
+			FROM (
+				SELECT *
+				FROM prioridades_rango_presuntiva_tmp
+				WHERE usuario = p_usuario
+			) y
+			WHERE COALESCE(x.monto_presuntiva::numeric,0) BETWEEN COALESCE(y.monto_minimo,0) AND COALESCE(y.monto_maximo,x.monto_presuntiva::numeric)
+			AND x.usuario = p_usuario;
+	END IF;
 		
 	IF COALESCE(v_validador,0) > 0 THEN v_retorno := 1; ELSE v_retorno := 0; END IF;
 	
--- trunk/scripts/PROCEDURE/PROC_CARGA_OFICIOS.sql
+++ trunk/scripts/PROCEDURE/PROC_CARGA_OFICIOS.sql
@@ -5,11 +5,15 @@ DECLARE
 BEGIN
 /*-----------REGISTRO DE NOTIFIICACIONES (OFICIOS)-----------------*/
 	INSERT INTO notificaciones
-		(num_oficio,fecha_oficio,id_prioridad,domicilio,referencia_ubicacion,id_estatus_notificacion,
-		reg_xlayout,creado_por,ip_registro)
+		(num_orden,num_oficio,fecha_oficio,id_insumo,id_bloque,monto_presuntiva,id_prioridad,domicilio,referencia_ubicacion,
+		id_estatus_notificacion,reg_xlayout,creado_por,ip_registro)
 	SELECT 
+		nt.num_orden,
 		nt.num_oficio,
 		TO_DATE(nt.fecha_oficio,'dd-mm-yyyy') AS fecha_oficio,
+		nt.id_insumo::numeric,
+		nt.id_bloque::numeric,
+		COALESCE(nt.monto_presuntiva::numeric,0) AS monto_presuntiva,
 		UPPER(TRIM(nt.prioridad)) AS id_prioridad,
 		UPPER(TRIM(nt.domicilio)) AS domicilio,
 		UPPER(TRIM(nt.referencia_ubicacion)) AS referencia_ubicacion,
@@ -21,6 +25,7 @@ BEGIN
 	WHERE UPPER(TRIM(nt.usuario)) = UPPER(TRIM(vusuario))					
 	ORDER BY nt.id_notificacion_tmp;
 
+	DELETE FROM prioridades_rango_presuntiva_tmp WHERE parse_text(usuario) = parse_text(vusuario);
 	DELETE FROM notificaciones_tmp WHERE parse_text(usuario) = parse_text(vusuario);
 END;
 $$ LANGUAGE plpgsql;
