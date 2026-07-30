2dcb0d1dfe915abd58478e3e42d03180855d2aa6
jesus velazquez <jvl.24.12.88@gmail.com>
2026-07-29 21:11:10 -0600

    ajuste

---
+++ trunk/scripts/QUERYS/script_grant.sql
@@ -0,0 +1,5 @@
+GRANT SELECT ON TABLE 
+	painani.usuarios, painani.estatus_notificacion,painani.notificaciones,painani.paquetes,
+	painani.paquetes_notificaciones,painani.periodos,painani.prioridades,painani.prioridades_rango_presuntiva_tmp,
+	painani.soportes_notificacion,painani.usuarios_complementos
+TO painani_consulta;
