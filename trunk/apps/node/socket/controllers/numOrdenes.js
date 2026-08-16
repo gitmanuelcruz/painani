0383e333cecbdcf6df1ab79fb8ec8fea4d05874a
jesus velazquez <jvl.24.12.88@gmail.com>
2026-08-16 01:31:50 -0600

    ajuste

--- trunk/apps/node/socket/controllers/numOrdenes.js
+++ trunk/apps/node/socket/controllers/numOrdenes.js
@@ -1,25 +1,31 @@
 const pool = require('../config/db');
 
 const getNumOrdenes = async (req,res) => {
+   const { idControlVersion } = req.body;
    const sql = `SELECT
-                  id_notificacion,
-                  fecha_oficio,
-                  num_orden,
-                  fecha_hora_notificado,
-                  id_estatus_notificacion
-               FROM notificaciones`;
+               	nn.id_notificacion,
+               	nc.id_control_version,
+               	nn.fecha_oficio,
+               	nn.num_orden,
+               	nn.fecha_hora_notificado,
+               	nn.id_estatus_notificacion,
+               	nc.id_estatus_notificacion_al_corte
+               FROM notificaciones nn
+               INNER JOIN notificaciones_cortes nc ON nn.id_notificacion = nc.id_notificacion
+               WHERE nc.id_control_version = $1`;
 
    let numOrdenes = []
-   await pool.query(sql,[]).then(result => {
+   await pool.query(sql,[idControlVersion]).then(result => {
       result.rows.map(item => {
          let datos = {
-            "idNotificacion": item.id_notificacion,
-            "fechaOficio": item.fecha_oficio,
-            "numOrden": item.num_orden.trim(),
-            "fechaHoraNotificado": item.fecha_hora_notificado,
-            "idEstatusNotificacion": item.id_estatus_notificacion.trim()
+            "id_control_version": item.id_control_version,
+            "id_notificacion": item.id_notificacion,
+            "num_orden": item.num_orden.trim(),
+            "fecha_oficio": item.fecha_oficio,
+            "fecha_hora_notificado": item.fecha_hora_notificado,
+            "id_estatus_notificacion": item.id_estatus_notificacion.trim(),
+            "id_estatus_notificacion_al_corte": item.id_estatus_notificacion_al_corte.trim()
          }
-
          numOrdenes.push(datos);
       });
    });
